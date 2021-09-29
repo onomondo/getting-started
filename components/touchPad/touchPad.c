5207b43461b3e0dca2b7151d0c2d55760f090c8d
Peter S. Bornerup <pb@onomondo.com>
2021-09-29 17:24:07 +0200

    adjusted the automatic threshold of the touch pad.

--- components/touchPad/touchPad.c
+++ components/touchPad/touchPad.c
@@ -11,17 +11,19 @@ EventBits_t touch_bit;
 
 int64_t timestamp = 0;
 
-void touchCallback(void* arg) {
+void touchCallback(void *arg)
+{
     uint32_t pad_intr = touch_pad_get_status();
     //clear interrupt
     touch_pad_clear_status();
-    if (eventGroup != NULL) {
+    if (eventGroup != NULL)
+    {
         bool touch = pad_intr & (1 << TOUCH_PAD_NUM6);
 
         BaseType_t pxHigherPriorityTaskWoken = pdFALSE;
         // ESP_LOGI(TAG, "Touch pad status: %d", pad_intr);
         int64_t now = esp_timer_get_time();
-        if (touch && (now - timestamp > 500000))  //.5 sec debounce
+        if (touch && (now - timestamp > 100000)) //.1 sec debounce
         {
             timestamp = now;
             xEventGroupSetBitsFromISR(eventGroup, touch_bit, &pxHigherPriorityTaskWoken);
@@ -29,7 +31,8 @@ void touchCallback(void* arg) {
     }
 }
 
-esp_err_t touchpad_init(EventGroupHandle_t* eventgroup, EventBits_t eventBits) {
+esp_err_t touchpad_init(EventGroupHandle_t *eventgroup, EventBits_t eventBits)
+{
     eventGroup = *eventgroup;
     touch_bit = eventBits;
 
@@ -51,17 +54,20 @@ esp_err_t touchpad_init(EventGroupHandle_t* eventgroup, EventBits_t eventBits) {
     // Initialize and start a software filter to detect slight change of capacitance.
     touch_pad_filter_start(TOUCHPAD_FILTER_TOUCH_PERIOD);
 
-    uint16_t touch_value;
-    touch_pad_read_filtered(TOUCH_PAD_NUM6, &touch_value);
+    uint16_t touch_value, threshold;
 
-    ESP_LOGI(TAG, "test init: touch pad [%d] val is %d", TOUCH_PAD_NUM6, touch_value);
+    touch_pad_read_filtered(TOUCH_PAD_NUM6, &touch_value);
+    threshold = (uint16_t)touch_value * 4. / 5.;
+    ESP_LOGI(TAG, "test init: touch pad [%d] val is %d, %d", TOUCH_PAD_NUM6, touch_value, threshold);
     //set interrupt threshold.
-    ESP_ERROR_CHECK(touch_pad_set_thresh(TOUCH_PAD_NUM6, touch_value * 1 / 3));
+
+    ESP_ERROR_CHECK(touch_pad_set_thresh(TOUCH_PAD_NUM6, threshold));
 
     touch_pad_isr_register(touchCallback, NULL);
     return touch_pad_intr_enable();
 }
-esp_err_t touch_deinit() {
+esp_err_t touch_deinit()
+{
     touch_pad_intr_disable();
     touch_pad_isr_deregister(touchCallback, NULL);
 
