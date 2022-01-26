849888c4fdcf2c9e6b596c2229a1e64688403005
Peter S. Bornerup <pb@onomondo.com>
2022-01-26 10:00:56 +0100

    mainly linting

--- main/onomondo_getting_started.c
+++ main/onomondo_getting_started.c
@@ -4,14 +4,15 @@
 
 #include <driver/adc.h>
 #include <string.h>
-#include "driver/rtc_io.h"
+
 #include "accelerometer.h"
+#include "driver/rtc_io.h"
 #include "esp32/clk.h"
 #include "esp_adc_cal.h"
+#include "esp_event.h"
 #include "esp_log.h"
 #include "esp_pm.h"
 #include "esp_sleep.h"
-#include "esp_event.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/event_groups.h"
 #include "nvs.h"
@@ -39,8 +40,7 @@
 // ESP_EVENT_DECLARE_BASE(CELLULAR_EVENT);
 ESP_EVENT_DEFINE_BASE(USER_EVENTS);
 
-enum user_events
-{
+enum user_events {
     CLEAR_FPLMN_EVENT,
     POWER_DOWN_EVENT,
     SEND_CONNECTORS_EVENT,
@@ -90,18 +90,15 @@ float get_batt_voltage();
 void configure_io();
 
 // GPIO interrupt handler
-static void IRAM_ATTR gpio_isr_handler(void *arg)
-{
+static void IRAM_ATTR gpio_isr_handler(void *arg) {
     uint32_t gpio_num = (uint32_t)arg;
     xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
 }
 
-void app_main(void)
-{
+void app_main(void) {
     // Initialize NVS. This will be needed for OTA portion...
     esp_err_t err = nvs_flash_init();
-    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND)
-    {
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
         // 1.OTA app partition table has a smaller NVS partition size than the non-OTA
         // partition table. This size mismatch may cause NVS initialization to fail.
         // 2.NVS partition contains data in new format and cannot be recognized by this version of code.
@@ -118,11 +115,10 @@ void app_main(void)
 
     // if low bat is detected go to deep sleep.
     // device wont wake up before connected to a charger
-    if (batt < 3.1 && batt > 1)
-    {
+    if (batt < 3.1 && batt > 1) {
         ESP_LOGI(TAG, "Low battery: %f", batt);
         esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_OFF);
-        esp_sleep_disable_wakeup_source(ESP_SLEEP_WAKEUP_ALL); // usb insert will reset and reenable device.
+        esp_sleep_disable_wakeup_source(ESP_SLEEP_WAKEUP_ALL);  // usb insert will reset and reenable device.
         esp_deep_sleep_start();
     }
 
@@ -144,7 +140,7 @@ void app_main(void)
     app_state.modem_initialized = 1;
 
     // critical error.
-    if (status != ESP_OK)
+    if (status != ESP_OK)  // restarts
         powerOff(1);
 
     // handles cellular events.
@@ -156,148 +152,133 @@ void app_main(void)
     return;
 }
 
-static void cellular_event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
-{
-    switch (event_id)
-    {
-    case CELLULAR_PPP_STARTED:
-        app_state.ppp_mode = 1;
-        break;
-    case CELLULAR_ATTACHED:
-        app_state.network_available = 1;
-        break;
-    case CELLULAR_PPP_STOPPED:
-        app_state.ppp_mode = 0;
-        break;
-    case CELLULAR_STOPPED_SEARCHING:
-        app_state.network_available = 0;
-        break;
-    case CELLULAR_SEARCHING:
-        app_state.network_available = 0;
-        break;
-    case CELLULAR_POWERED_DOWN:
-        ESP_LOGI(TAG, "Modem off");
-        powerOff(0);
-        break;
-    case CELLULAR_NOT_AVAILABLE:
-        //bad luck. Power off...
-        forcePowerDown();
-        break;
+static void cellular_event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
+    switch (event_id) {
+        case CELLULAR_PPP_STARTED:
+            app_state.ppp_mode = 1;
+            break;
+        case CELLULAR_ATTACHED:
+            app_state.network_available = 1;
+            break;
+        case CELLULAR_PPP_STOPPED:
+            app_state.ppp_mode = 0;
+            break;
+        case CELLULAR_STOPPED_SEARCHING:
+            app_state.network_available = 0;
+            break;
+        case CELLULAR_SEARCHING:
+            app_state.network_available = 0;
+            break;
+        case CELLULAR_POWERED_DOWN:
+            ESP_LOGI(TAG, "Modem off");
+            powerOff(0);
+            break;
+        case CELLULAR_NOT_AVAILABLE:
+            //bad luck. Power off...
+            forcePowerDown();
+            break;
     }
 }
 
-static void user_event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
-{
-
+static void user_event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
     //     CLEAR_FPLMN_EVENT,
     //     POWER_DOWN_EVENT,
     //     SEND_CONNECTORS_EVENT,
     //     SEND_DNS_EVENT
-    switch (event_id)
-    {
-    case CLEAR_FPLMN_EVENT:
-        if (!app_state.ppp_mode)
-            clearFPLMMN();
-        break;
-
-    case POWER_DOWN_EVENT:
-        ESP_LOGI(TAG, "Powering down");
-        closeSocket(); // Just in case. Safe to call w. no socket created
-        app_state.power_pressed = 1;
-        forcePowerDown();
-        break;
-
-    case SEND_CONNECTORS_EVENT:
-        if (!app_state.network_available)
+    switch (event_id) {
+        case CLEAR_FPLMN_EVENT:
+            if (!app_state.ppp_mode)
+                clearFPLMMN();
             break;
 
-        if (!app_state.ppp_mode)
-        {
-            //start ppp and schedule event again.
-            requestPPP();
-            vTaskDelay(pdMS_TO_TICKS(200));
-
-            // hacky reschedule so users don't have to press multiple times.
-            // ppp status is posted in default event loop, so we can't block this too long.
-            esp_event_post(USER_EVENTS, SEND_CONNECTORS_EVENT, NULL, 0, 1);
-
-            break;
-        }
-        openSocket("1.2.3.4", 4321);
-
-        char payload[100]; // = "Hello from onomondo...!";
-        float temp = 0, hum = 0;
-        float battery = get_batt_voltage();
-        int signal = getSignalQuality();
-
-        tmp_read(&temp, &hum);
-        sprintf(payload, "{\"battery\": %f,\"signal\": %d,\"temperature\": %f}", battery, signal, temp);
-        sendData(payload, strlen(payload), 0);
-        ESP_LOGI(TAG, "Transmit: %s", payload);
-        break;
-    case SEND_DNS_EVENT:
-        if (!app_state.network_available)
+        case POWER_DOWN_EVENT:
+            ESP_LOGI(TAG, "Powering down");
+            closeSocket();  // Just in case. Safe to call w. no socket created
+            app_state.power_pressed = 1;
+            forcePowerDown();
             break;
 
-        if (!app_state.ppp_mode)
-        {
-            //start ppp and schedule event again.
-            requestPPP();
-            vTaskDelay(pdMS_TO_TICKS(200));
+        case SEND_CONNECTORS_EVENT:
+            if (!app_state.network_available)
+                break;
 
-            // hacky reschedule so users don't have to press multiple times.
-            // ppp status is posted in default event loop, so we can't block this too long.
-            esp_event_post(USER_EVENTS, SEND_DNS_EVENT, NULL, 0, 1);
+            if (!app_state.ppp_mode) {
+                //start ppp and schedule event again.
+                requestPPP();
+                vTaskDelay(pdMS_TO_TICKS(200));
 
+                // hacky reschedule so users don't have to press multiple times.
+                // ppp status is posted in default event loop, so we can't block this too long.
+                esp_event_post(USER_EVENTS, SEND_CONNECTORS_EVENT, NULL, 0, 1);
+
+                break;
+            }
+            openSocket("1.2.3.4", 4321);
+
+            char payload[100];  // = "Hello from onomondo...!";
+            float temp = 0, hum = 0;
+            float battery = get_batt_voltage();
+            int signal = getSignalQuality();
+
+            tmp_read(&temp, &hum);
+            sprintf(payload, "{\"battery\": %f,\"signal\": %d,\"temperature\": %f}", battery, signal, temp);
+            sendData(payload, strlen(payload), 0);
+            ESP_LOGI(TAG, "Transmit: %s", payload);
             break;
-        }
+        case SEND_DNS_EVENT:
+            if (!app_state.network_available)
+                break;
 
-        //get random hostname
-        char host[30];
-        uint16_t timeMs = (uint16_t)esp_timer_get_time();
-        timeMs |= 0x000F;
-        sprintf(host, "%d.onomondo.com", timeMs);
+            if (!app_state.ppp_mode) {
+                //start ppp and schedule event again.
+                requestPPP();
+                vTaskDelay(pdMS_TO_TICKS(200));
 
-        dnsLookup(host);
-        break;
+                // hacky reschedule so users don't have to press multiple times.
+                // ppp status is posted in default event loop, so we can't block this too long.
+                esp_event_post(USER_EVENTS, SEND_DNS_EVENT, NULL, 0, 1);
+
+                break;
+            }
+
+            //get random hostname
+            char host[30];
+            uint16_t timeMs = (uint16_t)esp_timer_get_time();
+            timeMs |= 0x000F;
+            sprintf(host, "%d.onomondo.com", timeMs);
+
+            dnsLookup(host);
+            break;
     }
 }
 
-static void gpio_filter_task(void *arg)
-{
+static void gpio_filter_task(void *arg) {
     uint8_t btn[4] = {0, 0, 0, 0};
     TickType_t delay = portMAX_DELAY;
     uint32_t io_num = 0;
     uint32_t events[4] = {CLEAR_FPLMN_EVENT, POWER_DOWN_EVENT, SEND_CONNECTORS_EVENT, SEND_DNS_EVENT};
 
-    for (;;)
-    {
-        delay = (btn[0] || btn[1] || btn[2] || btn[3]) ? pdMS_TO_TICKS(200) : portMAX_DELAY; //if button marked as active wait at most 200 ms for debounce.
-        if (xQueueReceive(gpio_evt_queue, &io_num, delay))
-        {
-            switch (io_num)
-            {
-            case BTN_CLEAR:
-                btn[0] = 1;
-                break;
-            case BTN_POWER:
-                btn[1] = 1;
-                break;
-            case BTN_CONNECTORS:
-                btn[2] = 1;
-                break;
-            case BTN_DNS:
-                btn[3] = 1;
-                break;
+    for (;;) {
+        delay = (btn[0] || btn[1] || btn[2] || btn[3]) ? pdMS_TO_TICKS(200) : portMAX_DELAY;  //if button marked as active wait at most 200 ms for debounce.
+        if (xQueueReceive(gpio_evt_queue, &io_num, delay)) {
+            switch (io_num) {
+                case BTN_CLEAR:
+                    btn[0] = 1;
+                    break;
+                case BTN_POWER:
+                    btn[1] = 1;
+                    break;
+                case BTN_CONNECTORS:
+                    btn[2] = 1;
+                    break;
+                case BTN_DNS:
+                    btn[3] = 1;
+                    break;
             }
-        }
-        else
-        {
+        } else {
             // receive timeout. BTN must be stabilized.
-            for (size_t i = 0; i < 4; i++)
-            {
-                if (btn[i])
-                {
+            for (size_t i = 0; i < 4; i++) {
+                if (btn[i]) {
                     esp_event_post(USER_EVENTS, events[i], NULL, 0, 1);
                     btn[i] = 0;
                 }
@@ -306,8 +287,7 @@ static void gpio_filter_task(void *arg)
     }
 }
 
-void configure_io()
-{
+void configure_io() {
     gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
     xTaskCreate(gpio_filter_task, "gpio_handler", 2048, NULL, 10, NULL);
 
@@ -336,9 +316,7 @@ void configure_io()
     gpio_isr_handler_add(BTN_CONNECTORS, gpio_isr_handler, (void *)BTN_CONNECTORS);
 }
 
-void powerOff(uint32_t RTCSleepInS)
-{
-
+void powerOff(uint32_t RTCSleepInS) {
     esp_sleep_disable_wakeup_source(ESP_SLEEP_WAKEUP_ALL);
     esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_ON);
 
@@ -356,9 +334,8 @@ void powerOff(uint32_t RTCSleepInS)
     esp_sleep_enable_ext1_wakeup(mask, ESP_EXT1_WAKEUP_ANY_HIGH);
 
     // esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_FAST_MEM, ESP_PD_OPTION_OFF);
-    if (RTCSleepInS)
-    {
-        esp_sleep_enable_timer_wakeup((uint64_t)RTCSleepInS * (uint64_t)1000 * (uint64_t)1000); // 10 secs
+    if (RTCSleepInS) {
+        esp_sleep_enable_timer_wakeup((uint64_t)RTCSleepInS * (uint64_t)1000 * (uint64_t)1000);  // 10 secs
     }
 
     // power off leds
@@ -370,8 +347,7 @@ void powerOff(uint32_t RTCSleepInS)
     esp_deep_sleep_start();
 }
 
-void led_task(void *param)
-{
+void led_task(void *param) {
     //figure out the state of the connection and/or error state.
 
     //gpio_set_level(LED_LOGO, 1);
@@ -382,27 +358,22 @@ void led_task(void *param)
 
     uint32_t timing = 0;
     uint8_t led_ppp = 0, led_clear = 0, led_power = 1;
-    while (1)
-    {
-        if (app_state.modem_initialized)
-        {
+    while (1) {
+        if (app_state.modem_initialized) {
             led_clear = 1;
 
             //attached but no pdp context yet
-            if (app_state.network_available && !app_state.ppp_mode)
-            {
+            if (app_state.network_available && !app_state.ppp_mode) {
                 led_ppp = timing % 8 == 0 ? (led_ppp ? 0 : 1) : led_ppp;
             }
 
             //attached but and ppp context
-            if (app_state.network_available && app_state.ppp_mode)
-            {
+            if (app_state.network_available && app_state.ppp_mode) {
                 led_ppp = 1;
-                led_clear = 0; //clear no longer available...
+                led_clear = 0;  //clear no longer available...
             }
 
-            if (app_state.power_pressed)
-            {
+            if (app_state.power_pressed) {
                 led_power = timing % 16 == 0 ? (led_power ? 0 : 1) : led_power;
                 led_clear = 0;
                 led_ppp = 0;
@@ -420,8 +391,7 @@ void led_task(void *param)
     }
 }
 
-void init_adc()
-{
+void init_adc() {
     adc_gpio_init(ADC_UNIT_1, ADC1_CHANNEL_0);
     adc_char = calloc(1, sizeof(esp_adc_cal_characteristics_t));
     esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_11db, ADC_WIDTH_BIT_12, 1, adc_char);
@@ -429,19 +399,17 @@ void init_adc()
     adc1_config_channel_atten(ADC1_CHANNEL_1, ADC_ATTEN_11db);
 }
 
-float get_batt_voltage()
-{
+float get_batt_voltage() {
     int raw = adc1_get_raw(ADC1_CHANNEL_0);
 
-    return (float)esp_adc_cal_raw_to_voltage(raw, adc_char) * 2 / 1000.0; // x2 due to voltage division..
+    return (float)esp_adc_cal_raw_to_voltage(raw, adc_char) * 2 / 1000.0;  // x2 due to voltage division..
 }
 
-void watchdog_task(void *param)
-{
+void watchdog_task(void *param) {
     // last resort watchdog. If device has been on for too long we reboot it... ->
     ESP_LOGI(TAG, "Watchdog start");
-    vTaskDelay(pdMS_TO_TICKS(1000 * 60 * 10)); // 10 minutes
+    vTaskDelay(pdMS_TO_TICKS(1000 * 60 * 10));  // 10 minutes
 
     ESP_LOGI(TAG, "Watchdog timeout");
-    powerOff(1); //sleep one second and reboot.
+    powerOff(1);  //sleep one second and reboot.
 }
